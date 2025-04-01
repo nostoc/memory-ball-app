928c39ef93d93142a8b12de4f326459c603cc7bf
nostoc <nostochk@gmail.com>
2025-04-01 23:36:35 +0530

    updated backend env

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 // Generate JWT Token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE,
+    expiresIn: "30d",
   });
 };
 
