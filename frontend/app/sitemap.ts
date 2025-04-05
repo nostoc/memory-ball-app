4afefe908fd4b4eb4ae558d73653e7786da917da
Janitha Senani Karunarathna <janithakarunarathna12@gmail.com>
2025-04-05 18:39:32 +0530

    add more sitemap links

--- frontend/app/sitemap.ts
+++ frontend/app/sitemap.ts
@@ -5,20 +5,38 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://memoryball.online',
       lastModified: new Date(),
-      changeFrequency: 'always',
+      changeFrequency: 'daily',
       priority: 1,
     },
     {
       url: 'https://memoryball.online/about',
       lastModified: new Date(),
-      changeFrequency: 'always',
+      changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
       url: 'https://memoryball.online/auth/login',
       lastModified: new Date(),
-      changeFrequency: 'always',
+      changeFrequency: 'weekly',
       priority: 0.8,
     },
+    {
+      url: 'https://memoryball.online/guide',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    }
+    {
+      url: 'https://memoryball.online/privacy',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://memoryball.online/terms',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    }
   ]
 }
