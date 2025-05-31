dd75f04ffc19c2f65a7b1a00de47fce76dc2ad02
nostoc <nostochk@gmail.com>
2025-05-31 19:16:35 +0530

    implement APIs for public deck sharing

--- backend/routes/deckRoutes.js
+++ backend/routes/deckRoutes.js
@@ -6,8 +6,15 @@ const sessionRouter = require('./sessionRoutes');
 
 const router = express.Router();
 
+// Public routes that don't require authentication
+router.get('/public', deckController.getPublicDecks);
+router.get('/public/:id', deckController.getPublicDeck);
+
 // Protect all routes after this middleware
 router.use(protect);
+
+// Copy a public deck to user's collection (requires authentication)
+router.post('/copy/:id', deckController.copyDeck);
 
 // Re-route to card router if the URL contains cardId
 router.use('/:deckId/cards', cardRouter);
